433a77ea4754efd691a2129af9e34e72443adab1
smpmariselvam <117361181+smpmariselvam@users.noreply.github.com>
2023-07-26 10:05:44 +0530

    Independence Day 16 Task

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("Lets start the timer!!!");
+setTimeout(function(){
+    console.log("Timer 10 is finished");
+
+    setTimeout(()=>{
+      console.log("Timer 9 is finished");
+
+      setTimeout(()=>{
+        console.log("Timer 8 is finished");
+
+        setTimeout(()=>{
+            console.log("Timer 7 is finished");
+
+            setTimeout(()=>{
+                console.log("Timer 6 is finished");
+
+                setTimeout(()=>{
+                    console.log("Timer 5 is finished");
+
+                    setTimeout(()=>{
+                        console.log("Timer 4 is finished");
+
+                        setTimeout(()=>{
+                            console.log("Timer 3 is finished");
+
+                            setTimeout(()=>{
+                                console.log("Timer 2 is finished");
+
+                                setTimeout(()=>{
+                                    console.log("Timer 1 is finished");
+                                    
+                                         console.log("Happy Independence Day !!!!");
+                                  },5500);
+                              
+                              },5000);
+                          
+                          },4500);
+                      
+                      },4000);
+                  
+                  },3500);
+              
+        
+              },3000);
+          
+              
+          },2500);
+      
+      },2000);
+  
+    },1500);
+
+},1000)
